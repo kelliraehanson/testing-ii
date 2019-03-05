2c00da8f9692fa384f3c41ae1f71aadb48662e51
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-03-05 17:47:33 -0600

    finished mvp

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -4,6 +4,8 @@ import 'jest-dom/extend-expect';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+describe('<App />', () => {
+
 it('increments ball count by 1', () => { 
   const { getByTestId, getByText } = render(<App />);
   const button = getByTestId('ball-btn');
@@ -29,5 +31,32 @@ it('increments strike count by 1', () => {
   expect(strikeDisplayText).toHaveTextContent(/Strikes: 2/);
   fireEvent.click(button);
   expect(strikeDisplayText).toHaveTextContent(/Strikes: 0/);
+
+})
+
+it('increments strike by 1 on foul unless strikes === 2', () => { 
+  const { getByTestId, getByText } = render(<App />);
+  const button = getByTestId('foul-btn');
+  const strikeDisplayText = getByTestId('strikes');
+  fireEvent.click(button);
+  expect(strikeDisplayText).toHaveTextContent(/Strikes: 1/);
+  fireEvent.click(button);
+  expect(strikeDisplayText).toHaveTextContent(/Strikes: 2/);
+  fireEvent.click(button);
+  expect(strikeDisplayText).toHaveTextContent(/Strikes: 2/);
+
+})
+
+it('resets balls and strikes on hit', () => { 
+  const { getByTestId, getByText } = render(<App />);
+  const button = getByTestId('hit-btn');
+  const strikeDisplayText = getByTestId('strikes');
+  const ballDisplayText = getByTestId('balls');
+
+  fireEvent.click(button);
+  expect(strikeDisplayText).toHaveTextContent(/Strikes: 0/);
+  expect(ballDisplayText).toHaveTextContent(/Balls: 0/);
+
+})
 
 })
