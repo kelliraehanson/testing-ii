32e63e3637acc316e29b8ef6ea15ec81e2e6bb6f
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-03-05 18:43:36 -0600

    added css

--- baseball/src/components/dashboard/Dashboard.js
+++ baseball/src/components/dashboard/Dashboard.js
@@ -4,8 +4,14 @@ const Dashboard = props => {
     return (
         <div>
             <button data-testid="strike-btn" onClick={props.addStrike}>Strike</button>
+            <br />
+            <br />
             <button data-testid="ball-btn" onClick={props.addBall}>Ball</button>
+            <br />
+            <br />
             <button data-testid="hit-btn" onClick={props.hit}>Hit</button>
+            <br />
+            <br />
             <button data-testid="foul-btn" onClick={props.foul}>Foul</button>
             
         
--- baseball/src/components/display/Display.js
+++ baseball/src/components/display/Display.js
@@ -5,9 +5,14 @@ const Dashboard = props => {
         return (
             <>
                 <div className="display">
-                <h1>Baseball!</h1>
-                    <h3 data-testid="strikes">Strikes: {props.strikes}</h3>
-                    <h3 data-testid="balls">Balls: {props.balls}</h3>
+                <h1>A LEAGUE OF THEIR OWN</h1>
+                <h3>"Are you crying? Are you crying? There's no crying. There's no crying in baseball!"</h3>
+            
+                <img src="https://www.tvovermind.com/wp-content/uploads/2018/08/league1-750x450-750x450.jpg"
+                alt="photo2"></img>
+
+                    <h4 data-testid="strikes">Strikes: {props.strikes}</h4>
+                    <h4 data-testid="balls">Balls: {props.balls}</h4>
                 </div>
 
             </>
